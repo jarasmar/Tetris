624604f0aea9bca9a7b6873ef635f23b9c3124f4
Jara Santamaria <jarasmar@gmail.com>
2020-05-19 14:38:35 +0100

    add player structure

--- tetris.js
+++ tetris.js
@@ -28,4 +28,9 @@ function drawMatrix(matrix, offset) {
   });
 }
 
-drawMatrix(matrix, {x: 5, y:5});
+const player = {
+  pos: {x: 5, y: 5},
+  matrix: matrix,
+}
+
+drawMatrix(player.matrix, player.pos);

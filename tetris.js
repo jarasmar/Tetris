99e6dec5f70eec7e8aea672c5eb2e2dc0a010a5e
Jara Santamaria <jarasmar@gmail.com>
2020-05-19 14:03:32 +0100

    change context scale

--- tetris.js
+++ tetris.js
@@ -1,6 +1,10 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
+// Scale pieces x20
+context.scale(20, 20);
+
+// Canvas style
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
 

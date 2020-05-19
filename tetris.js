1c59d9bc08bf2ce5cf9e290123ac5db8bf176bdf
Jara Santamaria <jarasmar@gmail.com>
2020-05-19 13:59:57 +0100

    add matrix for T piece and filling

--- tetris.js
+++ tetris.js
@@ -3,3 +3,20 @@ const context = canvas.getContext('2d');
 
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+// Piece on 'T' Shape
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0]
+];
+
+// Draw 'T' Piece
+matrix.forEach((row, y) => {
+  row.forEach((value, x) => {
+    if (value !== 0) {
+      context.fillStyle = 'red';
+      context.fillRect(x, y, 1, 1);
+    }
+  });
+});

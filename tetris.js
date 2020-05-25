ea81dd85330211083089441936c8ff0abf50d3f3
Jara Santamaria <jarasmar@gmail.com>
2020-05-25 17:25:02 +0100

    add a different colour for each piece

--- tetris.js
+++ tetris.js
@@ -18,7 +18,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x, 
                         y + offset.y, 
                         1, 1);
@@ -47,38 +47,38 @@ function createPiece(type ) {
     ];
   } else if (type === 'O') {
     return [
-      [1, 1],
-      [1, 1],
+      [2, 2],
+      [2, 2],
     ];
   } else if (type === 'L') {
     return [
-      [0, 1, 0],
-      [0, 1, 0],
-      [0, 1, 1],
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3],
     ];
   } else if (type === 'J') {
     return [
-      [0, 1, 0],
-      [0, 1, 0],
-      [1, 1, 0],
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0],
     ];
   } else if (type === 'I') {
     return [
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
     ];
   } else if (type === 'S') {
     return [
-      [0, 1, 1],
-      [1, 1, 0],
+      [0, 6, 6],
+      [6, 6, 0],
       [0, 0, 0],
     ];
   } else if (type === 'Z') {
     return [
-      [1, 1, 0],
-      [0, 1, 1],
+      [7, 7, 0],
+      [0, 7, 7],
       [0, 0, 0],
     ]; 
   }  
@@ -204,6 +204,17 @@ function update(time = 0) {
   draw();
   requestAnimationFrame(update);
 }
+
+const colors = [
+  null,
+  '#FF0000',
+  '#FF8C00',
+  '#FFFF00',
+  '#00FF00',
+  '#00FFFF',
+  '#0000FF',
+  '#FF00FF',
+];
 
 const arena = createMatrix(12, 20);
 // console.log(arena); console.table(arena);

3481a860e364fc5df2b05afb6a7e4d5cc43d0f71
Jara Santamaria <jarasmar@gmail.com>
2020-05-25 16:47:28 +0100

    add shapes for all the different pieces

--- tetris.js
+++ tetris.js
@@ -43,9 +43,45 @@ function createPiece(type ) {
     return [
       [0, 0, 0],
       [1, 1, 1],
-      [0, 1, 0]
+      [0, 1, 0],
     ];
-  }
+  } else if (type === 'O') {
+    return [
+      [1, 1],
+      [1, 1],
+    ];
+  } else if (type === 'L') {
+    return [
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 1, 1],
+    ];
+  } else if (type === 'J') {
+    return [
+      [0, 1, 0],
+      [0, 1, 0],
+      [1, 1, 0],
+    ];
+  } else if (type === 'I') {
+    return [
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+    ];
+  } else if (type === 'S') {
+    return [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ];
+  } else if (type === 'Z') {
+    return [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ]; 
+  }  
 }
 
 // copy values from player at arena in right position

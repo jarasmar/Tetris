1434f2d4f4326ba6448dcd59e6af3111a3c176ed
Jara Santamaria <jarasmar@gmail.com>
2020-05-26 18:09:44 +0100

    refactor code to have a constructor prototype structure

--- tetris.js
+++ tetris.js
@@ -1,29 +1,41 @@
 'use strict';
 
 function Tetris() {
-  this.canvas = document.getElementById('tetris');
-  this.context = canvas.getContext('2d');
 };
 
+const canvas = document.getElementById('tetris');
 
+const context = canvas.getContext('2d');
 
-// const canvas = document.getElementById('tetris');
-// const context = canvas.getContext('2d');
+const matrix = []
 
-// Scale pieces x20
+const colors = [
+  null,
+  '#FF0000',
+  '#FF8C00',
+  '#FFFF00',
+  '#00FF00',
+  '#00FFFF',
+  '#0000FF',
+  '#FF00FF',
+];
+  
+const player = {
+  pos: {x: 0, y: 0},
+  matrix: null,
+  score: 0,
+};
 
-
-// create sections in the background to hold the pieces at bottom
-function createMatrix(w, h) {
-  const matrix = []
-  // while height is not zero, decrease
+// create sections in the background to hold the pieces
+Tetris.prototype.createMatrix = function(w, h) {
   while (h--) {
     matrix.push(new Array(w).fill(0));
   }
   return matrix;
 }
 
-function drawMatrix(matrix, offset) {
+// Fill matrix with shapes
+Tetris.prototype.drawMatrix = function(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
@@ -36,18 +48,18 @@ function drawMatrix(matrix, offset) {
   });
 }
 
-function draw() {
+Tetris.prototype.draw = function() {
   // Canvas style
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
   
   // draws the old players in arena
-  drawMatrix(arena, {x: 0, y: 0});
+  tetris.drawMatrix(arena, {x: 0, y: 0});
   // draws the moving player
-  drawMatrix(player.matrix, player.pos);
+  tetris.drawMatrix(player.matrix, player.pos);
 }
 
-function createPiece(type ) {
+Tetris.prototype.createPiece = function(type) {
   if (type === 'T') {
     return [
       [0, 0, 0],
@@ -93,8 +105,8 @@ function createPiece(type ) {
   }  
 }
 
-// copy values from player at arena in right position
-function merge(arena, player) {
+// Keep shapes in arena
+Tetris.prototype.merge = function(arena, player) {
   player.matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
@@ -104,7 +116,8 @@ function merge(arena, player) {
   });
 };
 
-function collide(arena, player) {
+// Check for shapes collisions
+Tetris.prototype.collide = function(arena, player) {
   // rename player matrix and position
   const [m, o] = [player.matrix, player.pos];
   
@@ -125,7 +138,7 @@ function collide(arena, player) {
 }
 
 // clear full lines in arena
-function arenaSweep() {
+Tetris.prototype.arenaSweep = function() {
   let rowCount = 1;
   outer: for (let y = arena.length - 1; y > 0; --y) {
     for (let x = 0; x < arena[y].length;  ++x) {
@@ -144,61 +157,62 @@ function arenaSweep() {
   }
 }
 
-function playerDrop() {
+// Control pieces movements
+Tetris.prototype.playerDrop = function() {
   player.pos.y++;
-  if (collide(arena, player)) {
+  if (tetris.collide(arena, player)) {
     // move the player one up, merge with arena and move back to the top to start again
     player.pos.y--;
-    merge(arena, player);
-    playerReset();
-    arenaSweep();
-    updateScore();
+    tetris.merge(arena, player);
+    tetris.playerReset();
+    tetris.arenaSweep();
+    tetris.updateScore();
   }
   dropCounter = 0;
 }
 
-function playerMove(dir) {
+Tetris.prototype.playerMove = function(dir) {
   // doesn't allow collision with the sides or other players
   player.pos.x += dir;
-  if (collide(arena, player)) {
+  if (tetris.collide(arena, player)) {
     player.pos.x -= dir;
   }
 }
 
 // New pieces are random and appear top center
-function playerReset() {
+Tetris.prototype.playerReset = function() {
   const pieces = 'ILJOTSZ'; 
-  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+  player.matrix = tetris.createPiece(pieces[pieces.length * Math.random() | 0]);
   player.pos.y = 0;
   player.pos.x = (arena[0].length / 2 | 0) -
                   (player.matrix[0].length / 2 | 0);
 
   // if we collide inmediately with a new piece: game over (clean arena and reset score)
-  if (collide(arena, player)) {
+  if (tetris.collide(arena, player)) {
     arena.forEach(row => row.fill(0));
     player.score = 0;
-    updateScore();
+    tetris.updateScore();
   }
 }
 
-function playerRotate(dir) {
+Tetris.prototype.playerRotate = function(dir) {
   const pos = player.pos.x;
   let offset = 1;
 
-  rotate(player.matrix, dir);
+  tetris.rotate(player.matrix, dir);
 
-  while (collide(arena, player)) {
+  while (tetris.collide(arena, player)) {
     player.pos.x += offset;
     offset = -(offset + (offset > 0 ? 1 : -1));
     if (offset > player.matrix[0].length) {
-      rotate(player.matrix, -dir);
+      tetris.rotate(player.matrix, -dir);
       player.pos.x = pos;
       return;
     }
   }
 }
 
-function rotate(matrix, dir) {
+Tetris.prototype.rotate = function(matrix, dir) {
   for (let y = 0; y < matrix.length; ++y) {
     for (let x =0; x < y; ++x) {
       [
@@ -224,63 +238,47 @@ let dropInterval = 1000;
 
 let lastTime = 0;
 
-function update(time = 0) {
+// Changes piece position automatically
+Tetris.prototype.update = function(time = 0) {
   const deltaTime = time - lastTime;
   lastTime = time;
 
   // Everytime a second passes, piece drops one position
   dropCounter += deltaTime;
   if (dropCounter > dropInterval) {
-    playerDrop();
+    tetris.playerDrop();
   }
 
-  draw();
-  requestAnimationFrame(update);
+  tetris.draw();
+  requestAnimationFrame(tetris.update);
 }
 
-function updateScore() {
+Tetris.prototype.updateScore = function() {
   document.getElementById('score').innerText = player.score;
 }
-
-const colors = [
-  null,
-  '#FF0000',
-  '#FF8C00',
-  '#FFFF00',
-  '#00FF00',
-  '#00FFFF',
-  '#0000FF',
-  '#FF00FF',
-];
-
-const arena = createMatrix(12, 20);
-// console.log(arena); console.table(arena);
-
-const player = {
-  pos: {x: 0, y: 0},
-  matrix: null,
-  score: 0,
-};
-
 
 document.addEventListener('keydown', event => {
   // Find keyCode for arrows
   // console.log(event);
 
   if (event.keyCode === 37) {
-    playerMove(-1)
+    tetris.playerMove(-1)
   } else if (event.keyCode === 39) {
-    playerMove(1)
+    tetris.playerMove(1)
   } else if (event.keyCode === 40) {
-    playerDrop();
+    tetris.playerDrop();
   } else if (event.keyCode === 81) {
-    playerRotate(-1);
+    tetris.playerRotate(-1);
   } else if (event.keyCode === 87) {
-    playerRotate(1);
+    tetris.playerRotate(1);
   }
 });
 
-this.context.scale(20, 20);
-playerReset();
-updateScore();
-update();
+// Scale pieces x20
+context.scale(20, 20);
+
+let tetris = new Tetris;
+const arena = tetris.createMatrix(12, 20);
+tetris.playerReset();
+tetris.updateScore();
+tetris.update();
